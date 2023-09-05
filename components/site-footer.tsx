3a6d889974585ce74fb4b6afb5c6ac0f156092ff
S4ntiego <adamksiazek123@gmail.com>
2023-09-05 07:13:51 +0200

    footer change

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,25 +3,7 @@ export function SiteFooter() {
     <footer className="py-6 md:px-8 md:py-0 border-t">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-20 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{" "}
-          <a
-            href={"elo"}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            shadcn
-          </a>
-          . The source code is available on{" "}
-          <a
-            href={"elo"}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            GitHub
-          </a>
-          .
+          Built by Adam Ksiazek
         </p>
       </div>
     </footer>
