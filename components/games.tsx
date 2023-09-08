c0b0af4768f40dd94d56c7c0cc3313117e01f211
S4ntiego <adamksiazek123@gmail.com>
2023-09-08 23:35:51 +0200

    added shimmer loading for image

--- components/games.tsx
+++ components/games.tsx
@@ -62,7 +62,7 @@ const Games = () => {
     }
 
     return (
-      <div className="pagination mb-8 flex justify-center items-center space-x-2">
+      <div className="pagination pt-4 md:pt-0 mb-8 flex justify-center items-center space-x-2">
         {pageNumbers.map((number) => (
           <Button
             variant="outline"
@@ -162,6 +162,25 @@ const Games = () => {
     setFilteredGames,
   ]);
 
+  const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+  const toBase64 = (str: string) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+
   return (
     <div className="flex flex-col" ref={gridRef}>
       <div className="grid grid-cols-2 lg:grid-cols-5 gap-4 lg:gap-8 py-4 lg:py-6">
@@ -172,8 +191,11 @@ const Games = () => {
                 key={game.ProductId}
                 src={`/resized_images/${game.ProductId}_resized.jpg`}
                 alt={game.ProductTitle}
-                width={256}
-                height={188}
+                placeholder={`data:image/svg+xml;base64,${toBase64(
+                  shimmer(188, 256)
+                )}`}
+                width={188}
+                height={256}
                 className="rounded-[4px]"
               />
               <div className="absolute bottom-1.5 w-full flex justify-between px-1.5">
