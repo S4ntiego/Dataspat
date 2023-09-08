f5684f14f37d6b14dbdb4f753f2d54675addb8fc
S4ntiego <adamksiazek123@gmail.com>
2023-09-08 23:42:50 +0200

    shimmer height test

--- components/games.tsx
+++ components/games.tsx
@@ -163,7 +163,7 @@ const Games = () => {
   ]);
 
   const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg width="${w}" height="${400}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
       <stop stop-color="#333" offset="20%" />
@@ -171,8 +171,8 @@ const Games = () => {
       <stop stop-color="#333" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <rect width="${w}" height="${400}" fill="#333" />
+  <rect id="r" width="${w}" height="${400}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`;
 
